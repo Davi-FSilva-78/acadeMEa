e23ab46a31c71f018bf9977bbb4b2bd02b3cafe8
Davi Silva <davi.fsilva@sptech.school>
2025-05-18 23:18:11 -0300

    feat: add post-login navigation pages and initialize sUPino clicker game logic

---
+++ public/components/header/user_header.js
@@ -0,0 +1,18 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <div class="logo"></div>
+                <nav class="visit_header_container">
+                    <ul class="list_visit_option">
+                        <li class="cadastro"> <a href="./cadastro.html">Cadastro</a></li>
+                        |
+                        <li class="login"> <a href="./login.html">Login</a></li>     
+                    </ul>
+                </nav>
+            </header>
+        `;
+    }
+}
+
+customElements.define('user-header', Header);
