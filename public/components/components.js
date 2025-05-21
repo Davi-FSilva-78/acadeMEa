46d779b2779bfea8a8872da67a8ffb92433199f5
Davi Silva <davi.fsilva@sptech.school>
2025-05-20 21:39:07 -0300

    feat: finish the 'sUPino' game logic and add navigation bar with page selection burger

---
+++ public/components/components.js
@@ -0,0 +1,8 @@
+
+    function openNav() {
+        myNav.style.width = '40%'
+    }
+
+    function closeNav() {
+        myNav.style.width = '0'
+    }
--- public/components/header/user_header.js
+++ public/components/header/user_header.js
@@ -1,16 +1,34 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header>
-                <div class="logo"></div>
-                <nav class="visit_header_container">
-                    <ul class="list_visit_option">
-                        <li class="cadastro"> <a href="./cadastro.html">Cadastro</a></li>
-                        |
-                        <li class="login"> <a href="./login.html">Login</a></li>     
-                    </ul>
-                </nav>
-            </header>
+        <header>
+
+            <nav class="overlay" id="myNav">
+                <a href="javascript:void(0)" class="closeBtn" onclick="closeNav()">&times;</a>
+                <div class="overlaycontent">
+                    <a href="../../locked/index.html">Home</a>
+                    <hr>
+                    <a href="../../locked/previas.html">Previas</a>
+                    <a href="../../locked/dicas.html">Dicas</a>
+                    <a href="../../locked/curiosidades.html">curiosidades</a>
+                    <hr>
+                    <a href="../../locked/clickerGame.html">Jogos</a>
+                    <a href="../../locked/dashboard/dashboard.html">Dashboard</a>
+                    
+                </div>
+            </nav>
+            
+            <div class="logo"></div>
+            <nav class="visit_header_container">
+                <div class="list_visit_option">
+                        <div class="burguerSalas" onclick="openNav()">
+                            <div>-</div>
+                            <div>-</div>
+                            <div>-</div>
+                        </div>
+                </div>
+            </nav>
+        </header>
         `;
     }
 }
